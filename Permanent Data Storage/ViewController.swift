2dbcfd756c849ccd359dd03b55952963df8ed23e
Tad <terry.donaghe@gmail.com>
2016-10-15 11:38:47 -0700

    First commit

--- Permanent Data Storage/ViewController.swift
+++ Permanent Data Storage/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtPhoneNumber: UITextField!
+    @IBOutlet weak var lblStoredPhoneNumber: UILabel!
+    
+    
+    
+    @IBAction func btnRemember(_ sender: AnyObject) {
+        if !(txtPhoneNumber.text?.isEmpty)! {
+            UserDefaults.standard.set(txtPhoneNumber.text, forKey: "phoneNumber")
+            lblStoredPhoneNumber.text = txtPhoneNumber.text
+        } else {
+            lblStoredPhoneNumber.text = "Please enter a phone number above."
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let storedNumberObject = UserDefaults.standard.object(forKey: "phoneNumber")
+        if let storedNumber = storedNumberObject as? String {
+            lblStoredPhoneNumber.text = storedNumber
+        } else {
+            lblStoredPhoneNumber.text = ""
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
